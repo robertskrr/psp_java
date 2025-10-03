be17cffd99b91515b1cb7d041e0e89aed0bdf64d
Robert Esquerre <esquerre.valiente.robert.ernesto@iescamas.es>
2025-10-03 20:39:02 +0200

    Creado Utils.java

---
+++ PSP_UD1/src/psp/Utils.java
@@ -0,0 +1,14 @@
+package psp;
+
+import java.util.*;
+
+public class Utils {
+	
+	public static boolean isWindows() {
+		return System.getProperty("os.name").toLowerCase().contains("win");
+	}
+
+	public static List<String> sh(String cmd) {
+		return isWindows() ? Arrays.asList("cmd", "/c", cmd) : Arrays.asList("sh", "-c", cmd);
+	}
+}
